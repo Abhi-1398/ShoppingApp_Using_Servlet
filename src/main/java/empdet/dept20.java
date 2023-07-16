37542c1c8abfbbd13623077e9bee04d1cfa9f4fd
Abhi-1398 <136895740+Abhi-1398@users.noreply.github.com>
2023-07-16 23:34:32 +0530

    ProjectCommit

---
+++ src/main/java/empdet/dept20.java
@@ -0,0 +1,74 @@
+package empdet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dept20")
+public class dept20 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Connection con;
+	public void init(ServletConfig config) throws ServletException {
+		con = (Connection)config.getServletContext().getAttribute("jdbccon");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 try {
+
+        	 Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        	 ResultSet rs = st.executeQuery("select * from emp where deptno =20");
+        	 
+        	 response.getWriter().print("<table border =1>");
+        	 
+        	 if(rs.next())
+        	 {
+        		 rs.absolute(0);
+        		 while(rs.next())
+        		 {
+        		 response.getWriter().print("<tr>");
+        		 
+        		 	response.getWriter().print("<td>");
+        		 
+                  		response.getWriter().print(rs.getString(1));
+                  
+                  	response.getWriter().print("</td>");
+        		 
+                    response.getWriter().print("<td>");
+        		 
+                        response.getWriter().print(rs.getString(2));
+                  
+        		    response.getWriter().print("</td>");
+        		 
+        		 response.getWriter().print("</tr>");
+        	 
+        		 }
+        	 }
+        	 else
+        	 {
+        		 response.getWriter().print("No record fetched");
+        	 }
+        	 
+        	 response.getWriter().print("</table>");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		 String url = "http://localhost:8080/StudentProject/StudentCourse.jsp";
+         response.getWriter().print("<a href="+url+" >Click for Main Page</a>");
+	}
+
+}
---
+++ src/main/webapp/StudentCourse.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>StudentKatta</title>
+</head>
+<body>
+
+   <h1>Below are Department</h1>
+   <a href="http://localhost:8080/StudentProject/dept10" >DeptNO: 10</a><br/>
+   <a href="http://localhost:8080/StudentProject/dept20" >DeptNO: 20</a><br/>
+   <a href="http://localhost:8080/StudentProject/dept30" >DeptNO: 30</a><br/>
+   <a href="http://localhost:8080/StudentProject/dept40" >DeptNO: 40</a>
+
+</body>
+</html>
